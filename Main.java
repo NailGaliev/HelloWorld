c78b3b2651f52c737189e23bbbd772feaf0bf460
NailGaliev <55915785+NailGaliev@users.noreply.github.com>
2020-03-12 18:19:57 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main (String [] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int [] arr = new int [sc.nextInt()];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        VectorCode nail = new VectorCode(arr);
+        nail.print();
+        int [] gail = nail.decode();
+        for (int i = 0; i < gail.length; i++) {
+            System.out.print(gail[i] + " ");
+        }
+        System.out.println();
+        nail.delete(6);
+        nail.print();
+       // nail.insert(228, 4);
+      //  nail.print();
+        VectorCode nail2 = new VectorCode(arr);
+       // System.out.println(nail.scalarProduct(nail2));
+        nail.mult(1,4);
+       // nail.print();
+        VectorCode nail3 = new VectorCode(arr);
+        //System.out.println(nail3.sum(nail3));
+    }
+}
